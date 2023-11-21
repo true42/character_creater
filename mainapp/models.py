ed7567ed0deaa757518a4c6d007b893de3a7ef75
true42 <bezmaternykh.alexey@gmail.com>
2023-11-21 21:12:01 +0300

    add urls and index.html

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Race:
+    race_name = 'name_race'
+    description = 'description_race'
+    bonus = ''
+
+    def __init__(self, *args):
+        pass
+
+    def __str__(self):
+        return f'{self.race_name}'
+
+    def get_description(self, name_race):
+        pass
+
+
+class Class:
+    class_name = ''
+    description_name = ''
+
+    def __init__(self, *args):
+        pass
+
+    def __str__(self):
+        return f'{self.class_name}'
